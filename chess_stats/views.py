7167dfdf707928b2b499717bb9f4c696a54ed4ac
Ivan Malison <IvanMalison@gmail.com>
2013-02-03 18:04:53 -0800

    Changed view logic a bit.

--- chess_stats/views.py
+++ chess_stats/views.py
@@ -8,14 +8,15 @@ from django.views.generic import TemplateView
 import logic
 
 
+username_matcher = re.compile('([^/]*?)$')
+
+
 class UserGameBrowserView(TemplateView):
 	template_name = (os.path.dirname(__file__) + '/templates/browse_games.html')
 
-	username_matcher = re.compile('([^/]*?)$')
-
 	def get_context_data(self, **kwargs):
 		context = super(UserGameBrowserView, self).get_context_data(**kwargs)
-		username = self.username_matcher.search(self.request.path).group(1)
+		username = username_matcher.search(self.request.path).group(1)
 		context['games'] = logic.fetch_games_for_user(username)
 		return context
 
@@ -23,11 +24,22 @@ class UserGameBrowserView(TemplateView):
 class UserMoveBrowserView(TemplateView):
 	template_name = (os.path.dirname(__file__) + '/templates/browse_moves.html')
 
+	def get_context_data(self, **kwargs):
+		context = super(UserMoveBrowserView, self).get_context_data(**kwargs)
+		username = username_matcher.search(self.request.path).group(1)
+		context['username'] = username
+		return context
+
 
 def get_game_stats(request):
 	username = request.GET['username']
 	moves = simplejson.loads(request.GET['moves'])
-	json = simplejson.dumps(
-		logic.build_sorted_game_stats_for_moves_by_username(username, moves)
-	)
+	if username:
+		move_stats = logic.build_sorted_game_stats_for_moves_by_username(
+			username,
+			moves
+		)
+	else:
+		move_stats = logic.build_sorted_game_stats_for_moves_for_all_games(moves)
+	json = simplejson.dumps(move_stats)
 	return HttpResponse(json)
