51a0864571d0bca18769159a6a41d27b8253ddc4
Ivan Malison <IvanMalison@gmail.com>
2013-02-20 23:08:16 -0800

    using postgres instead of sqlite.

--- chess_stats/__init__.py
+++ chess_stats/__init__.py
@@ -3,10 +3,7 @@ import os
 from flask import Flask, url_for
 
 
-application_directory = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 app.jinja_env.globals['get_static_url'] = lambda filename: url_for('static', filename=filename)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///{app_dir}/testing.db'.format(
-	app_dir=application_directory
-)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 #app.debug = True
