497e775ddd1eb3b00fb86837e0ecfa0fb23b6bb2
Ivan Malison <IvanMalison@gmail.com>
2013-02-20 22:31:10 -0800

    Fixed some stuff for heroku.

---
+++ util.py
@@ -0,0 +1,22 @@
+import argparse
+
+def parse_host_and_port():
+	parser = argparse.ArgumentParser()
+	parser.add_argument(
+		'--port',
+		action='store',
+		type=int,
+		dest='port',
+		default=80,
+		help='The port that should be listned on.',
+	)
+	parser.add_argument(
+		'--host',
+		action='store',
+		type=str,
+		dest='host',
+		default='0.0.0.0',
+		help='The IP address on which to run the server.'
+	)
+	namespace = parser.parse_args()
+	return namespace.host, namespace.port
--- websocket_server.py
+++ websocket_server.py
@@ -2,9 +2,11 @@
 from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
 
+from util import parse_host_and_port
 from chess_stats import tornado
 
 if __name__ == '__main__':
+	host, port = parse_host_and_port()
 	http_server = HTTPServer(tornado.application)
-	http_server.listen(8888)
+	http_server.listen(port)
 	IOLoop.instance().start()
