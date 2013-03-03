8ae62d7a693352267be97cf84690631b817b8e61
Ivan Malison <IvanMalison@gmail.com>
2013-03-02 16:22:57 -0800

    Fade ins work.

--- chess_stats/static/js/app.js
+++ chess_stats/static/js/app.js
@@ -2,20 +2,47 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module(
-    'myApp',
-    ['myApp.filters', 'myApp.services', 'myApp.directives'],
+var ChessStats = angular.module(
+    'ChessStats',
+    [
+    	'ChessStats.filters',
+    	'ChessStats.services',
+    	'ChessStats.directives'
+    ],
     function($interpolateProvider) {
         $interpolateProvider.startSymbol('[[');
         $interpolateProvider.endSymbol(']]');
     }
-).
-  config(
-      [
-          '$routeProvider',
-          '$locationProvider',
-          function($routeProvider, $locationProvider) {
-              $locationProvider.html5Mode(true);
-          }
-      ]
-  );
+).config(
+    [
+        '$routeProvider',
+        '$locationProvider',
+        function($routeProvider, $locationProvider) {
+            $locationProvider.html5Mode(true);
+        }
+    ]
+);
+
+ChessStats.directive(
+    'jqAnimate',
+    function(jQueryExpression, templateElement){ 
+        console.log('init')
+        return function(instanceElement){
+            console.log('test')
+            instanceElement.show('slow'); 
+        } 
+    }
+);
+
+ChessStats.directive('ngFade', function () {
+    return function (scope, element, attrs) {
+        element.css('display', 'none');
+        scope.$watch(attrs.ngFade, function (value) {
+            if (value) {
+                element.fadeIn(500);
+            } else {
+                element.fadeOut(100);
+            }
+        });
+    }
+});
