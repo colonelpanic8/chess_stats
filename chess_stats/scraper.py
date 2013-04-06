e7bf5764acc16648596bd63d3f1c0419a12a14b1
Ivan Malison <IvanMalison@gmail.com>
2013-04-06 16:09:33 -0700

    Styling for main pages updated.

--- chess_stats/scraper.py
+++ chess_stats/scraper.py
@@ -1,4 +1,4 @@
-ChessComPGNFileFinderfrom contextlib import closing
+from contextlib import closing
 import itertools
 import glob
 import os
@@ -10,135 +10,135 @@ from BeautifulSoup import BeautifulSoup
 
 class ChessDotComScraper(object):
 
-        ARCHIVE_BASE_PAGE_FORMAT = "home/game_archive?member={member}&show={game_type}"
-        BASE_URL = "http://www.chess.com/"
-        GAME_LINK_HTML_ELEMENT_FORMAT_STRING = "c14_row{row}_7"
-        GAME_LINK_RE_MATCH_STRING = "/{game_type}/game\?id=([0-9]*)"
+    ARCHIVE_BASE_PAGE_FORMAT = "home/game_archive?member={member}&show={game_type}"
+    BASE_URL = "http://www.chess.com/"
+    GAME_LINK_HTML_ELEMENT_FORMAT_STRING = "c14_row{row}_7"
+    GAME_LINK_RE_MATCH_STRING = "/{game_type}/game\?id=([0-9]*)"
 
-	GAME_TYPE_LIVE = 0
-        GAME_TYPE_ONLINE = 1
+    GAME_TYPE_LIVE = 0
+    GAME_TYPE_ONLINE = 1
 
-	GAME_TYPES_SHOW = {
-                GAME_TYPE_LIVE: "live",
-                GAME_TYPE_ONLINE: "echess"
-        }
+    GAME_TYPES_SHOW = {
+        GAME_TYPE_LIVE: "live",
+        GAME_TYPE_ONLINE: "echess"
+    }
 
-	GAME_TYPES_PATH = {
-                GAME_TYPE_LIVE: "livechess",
-                GAME_TYPE_ONLINE: "echess"
-        }
+    GAME_TYPES_PATH = {
+        GAME_TYPE_LIVE: "livechess",
+        GAME_TYPE_ONLINE: "echess"
+    }
 
-	def __init__(self, game_type, member, log_function=None):
-                self.member = member
-                self.game_ids = []
-                self.soup = None
-                self.game_type = game_type
-                self.done = False
-                if log_function is None:
-                        def log(string):
-                                print string
-                                log_function = log
-                                self.log_function = log_function
+    def __init__(self, game_type, member, log_function=None):
+        self.member = member
+        self.game_ids = []
+        self.soup = None
+        self.game_type = game_type
+        self.done = False
+        if log_function is None:
+            def log(string):
+                print string
+            log_function = log
+        self.log_function = log_function
 
-	def scrape(self, stop_at_id=None):
-                with closing(
-                                urlopen(
-                                        self.BASE_URL + self.ARCHIVE_BASE_PAGE_FORMAT.format(
-                                                member=self.member,
-                                                game_type=self.GAME_TYPES_SHOW[self.game_type]
-                                        )
-				)
-		) as html:
+    def scrape(self, stop_at_id=None):
+        with closing(
+                urlopen(
+                    self.BASE_URL + self.ARCHIVE_BASE_PAGE_FORMAT.format(
+                        member=self.member,
+                        game_type=self.GAME_TYPES_SHOW[self.game_type]
+                    )
+                )
+        ) as html:
+            self.soup = BeautifulSoup(html.read())
+            while not self.done:
+                for game_id in self.parse_page(stop_at_id=stop_at_id):
+                    yield game_id
+                    self.done = not self.find_next_page()
+
+    def parse_page(self, stop_at_id=None):
+        """Parse a single page of a game archive from chess.com. Returns true if
+        stop_at_id was encountered."""
+        for row in itertools.count():
+            game_id = self.get_game_id(row)
+            if game_id is None:
+                self.log_function("No more games found on page.")
+                raise StopIteration()
+            elif game_id == stop_at_id:
+                self.done = True
+                raise StopIteration()
+            else:
+                self.game_ids.append(game_id)
+                yield game_id
+
+    def find_next_page(self):
+        """Find the next page from the html of the current page. Returns True on
+        success and False on failure.
+        """
+
+        pagination_find = self.soup.find(
+            name="ul",
+            attrs={"class" : "pagination"}
+        )
+        if pagination_find:
+            links = pagination_find.findAll(name = "a")
+            for link in links:
+                if re.match('^Next', link.contents[0]):
+                    with closing(urlopen(self.BASE_URL + link['href'])) as html:
                         self.soup = BeautifulSoup(html.read())
-                        while not self.done:
-                                for game_id in self.parse_page(stop_at_id=stop_at_id):
-                                        yield game_id
-                                        self.done = not self.find_next_page()
+                        return True
+                        self.log_function('Could not find a "Next" link.')
+                        return False
+        else:
+            self.log_function("Could not find pagination.")
+            return False
 
-	def parse_page(self, stop_at_id=None):
-                """Parse a single page of a game archive from chess.com. Returns true if
-                stop_at_id was encountered."""
-                for row in itertools.count():
-                        game_id = self.get_game_id(row)
-                        if game_id is None:
-                                self.log_function("No more games found on page.")
-                                raise StopIteration()
-                        elif game_id == stop_at_id:
-                                self.done = True
-                                raise StopIteration()
-                        else:
-                                self.game_ids.append(game_id)
-                                yield game_id
+    def get_game_id(self, row):
+        """Get one game id from the current page at the row stored in row."""
 
-	def find_next_page(self):
-                """Find the next page from the html of the current page. Returns True on
-                success and False on failure.
-                """
+        find_results = self.soup.find(
+            name='td',
+            id=self.GAME_LINK_HTML_ELEMENT_FORMAT_STRING.format(row=row)
+        )
+        if not find_results:
+            return None
 
-                pagination_find = self.soup.find(
-                        name="ul",
-                        attrs={"class" : "pagination"}
-                )
-                if pagination_find:
-                        links = pagination_find.findAll(name = "a")
-                        for link in links:
-                                if re.match('^Next', link.contents[0]):
-                                        with closing(urlopen(self.BASE_URL + link['href'])) as html:
-                                                self.soup = BeautifulSoup(html.read())
-                                                return True
-                                                self.log_function('Could not find a "Next" link.')
-                                                return False
-                                        else:
-                                                self.log_function("Could not find pagination.")
-                                                return False
+        game_url = find_results.contents[0]['href']
 
-	def get_game_id(self, row):
-                """Get one game id from the current page at the row stored in row."""
+        re_match_string = self.GAME_LINK_RE_MATCH_STRING.format(
+            game_type=self.GAME_TYPES_PATH[self.game_type]
+        )
+        match = re.search(
+            re_match_string,
+            game_url
+        )
+        if match:
+            game_id = int(match.group(1))
+        else:
+            self.log_function("Could not match game link string")
+            return None
 
-		find_results = self.soup.find(
-                        name='td',
-                        id=self.GAME_LINK_HTML_ELEMENT_FORMAT_STRING.format(row=row)
-                )
-                if not find_results:
-                        return None
-
-                game_url = find_results.contents[0]['href']
-
-                re_match_string = self.GAME_LINK_RE_MATCH_STRING.format(
-                        game_type=self.GAME_TYPES_PATH[self.game_type]
-                )
-                match = re.search(
-                        re_match_string,
-                        game_url
-                )
-                if match:
-                        game_id = int(match.group(1))
-                else:
-                        self.log_function("Could not match game link string")
-                        return None
-
-                return game_id
+        return game_id
 
 
 class ChessComPGNFileFinder(object):
 
-	filename_matcher = re.compile('.*\.pgn')
+    filename_matcher = re.compile('.*\.pgn')
 
-	def get_matching_files(self, directory='.'):
-                for directory_path, directory_names, filenames in os.walk(directory):
-                        for filename in filenames:
-                                if self.filename_matcher(filename):
-                                        yield os.path.join(directory_path, filename)
+    def get_matching_files(self, directory='.'):
+        for directory_path, directory_names, filenames in os.walk(directory):
+            for filename in filenames:
+                if self.filename_matcher(filename):
+                    yield os.path.join(directory_path, filename)
 
-	def load(self, load_path=None, multi_game_pgns=False):
-                if not load_path:
-                        load_path = self.DEFAULT_LOAD_PATH
-                        if not os.path.exists(load_path):
-                                raise ValueError("load_path does not exist")
+    def load(self, load_path=None, multi_game_pgns=False):
+        if not load_path:
+            load_path = self.DEFAULT_LOAD_PATH
+            if not os.path.exists(load_path):
+                raise ValueError("load_path does not exist")
 
-                filenames = glob.glob(os.path.join(load_path, '*'))
-                filenames = (filename for filename in filenames if re.match(".*\.pgn", filename))
+        filenames = glob.glob(os.path.join(load_path, '*'))
+        filenames = (filename for filename in filenames if re.match(".*\.pgn", filename))
 
-                for filename in filenames:
-                        with open(filename) as pgn_file:
-                                yield pgn_file.read()
+        for filename in filenames:
+            with open(filename) as pgn_file:
+                yield pgn_file.read()
--- chess_stats/static/js/controllers.js
+++ chess_stats/static/js/controllers.js
@@ -21,8 +21,8 @@ function GameBrowseCtrl($scope) {
 
     gameLoader.requestGames = function () {
         var json_string = JSON.stringify({
-        	"type": "GET_GAMES",
-        	"username": this.username
+           "type": "GET_GAMES",
+           "username": this.username
         })
         gameLoader.webSocket.send(json_string)
     }
@@ -53,44 +53,44 @@ function GameBrowseCtrl($scope) {
 }
 
 function MoveStatsCtrl($scope, $http) {
-	var moveControl = {
-		moves: [],
-		movePairs: [],
-		moveStatsList: [],
-		username: "",
+   var moveControl = {
+      moves: [],
+      movePairs: [],
+      moveStatsList: [],
+      username: "",
         refreshLock: false,
         isWhite: true
-	}
+   }
 
-	moveControl.refreshMoveStatsList = function() {
+   moveControl.refreshMoveStatsList = function() {
         var self = this
-		$http(
-			{
-				'method': 'GET',
-				'url': '/chess_stats/get_stats',
-				'params': {
-					'moves': this.moves,
-					'username': this.username,
+      $http(
+         {
+            'method': 'GET',
+            'url': '/chess_stats/get_stats',
+            'params': {
+               'moves': this.moves,
+               'username': this.username,
                     'color': this.isWhite? 'white' : 'black'
-				}
-			}
-		).success(
-			function(newValue) {
-				moveControl.moveStatsList = newValue;
+            }
+         }
+      ).success(
+         function(newValue) {
+            moveControl.moveStatsList = newValue;
                 self.refreshLock = false
-			}
-		);
-		this.movePairs = this.getMovePairs()
-	}
+         }
+      );
+      this.movePairs = this.getMovePairs()
+   }
 
-	moveControl.addMove = function(move) {
+   moveControl.addMove = function(move) {
         if(this.refreshLock) {
             return
         }
         this.refreshLock = true
-		this.moves.push(move);
-		this.refreshMoveStatsList();
-	}
+      this.moves.push(move);
+      this.refreshMoveStatsList();
+   }
 
     moveControl.setColor = function(isWhite) {
         if(this.refreshLock) {
@@ -102,64 +102,64 @@ function MoveStatsCtrl($scope, $http) {
         this.refreshMoveStatsList();
     }
 
-	moveControl.setUsername = function(username) {
-		this.username = username;
-		this.moves = [];
-		this.refreshMoveStatsList();
-	}
+   moveControl.setUsername = function(username) {
+      this.username = username;
+      this.moves = [];
+      this.refreshMoveStatsList();
+   }
 
-	moveControl.removeLastNMoves = function(numMovesToRemove) {
+   moveControl.removeLastNMoves = function(numMovesToRemove) {
         if(this.refreshLock) {
             return
         }
         this.refreshLock = true
-		this.moves = this.moves.slice(0, numMovesToRemove*-1);
-		this.refreshMoveStatsList();
-	}
+      this.moves = this.moves.slice(0, numMovesToRemove*-1);
+      this.refreshMoveStatsList();
+   }
 
-	moveControl.truncateMovesTo = function(lastIndex) {
-		if(lastIndex >= this.moves.length) return
+   moveControl.truncateMovesTo = function(lastIndex) {
+      if(lastIndex >= this.moves.length) return
         if(this.refreshLock) {
             return
         }
         this.refreshLock = true
-		this.moves = this.moves.slice(0, lastIndex);
-		this.refreshMoveStatsList();
-	}
+      this.moves = this.moves.slice(0, lastIndex);
+      this.refreshMoveStatsList();
+   }
 
-	moveControl.getMovePairs = function() {
-		var movePairs = []
-		for(var i = 0; i < this.moves.length - 1; i += 2) {
-			movePairs.push(
-				{
-					'index': i,
-					'whiteMove': this.moves[i],
-					'blackMove': this.moves[i+1],
-				}
-			);
-		}
-		if(this.moves.length & 0x1 == 1) {			
-			movePairs.push(
-				{
-					'index': this.moves.length - 1,
-					'whiteMove': this.moves[this.moves.length - 1],
-					'blackMove': "..."
-				}
-			);
-		} else {
-			movePairs.push(
-				{
-					'index': this.moves.length,
-					'whiteMove': "...",
-					'blackMove': ""
-				}
-			);
-		};
-		return movePairs
-	}
+   moveControl.getMovePairs = function() {
+      var movePairs = []
+      for(var i = 0; i < this.moves.length - 1; i += 2) {
+         movePairs.push(
+            {
+               'index': i,
+               'whiteMove': this.moves[i],
+               'blackMove': this.moves[i+1],
+            }
+         );
+      }
+      if(this.moves.length & 0x1 == 1) {         
+         movePairs.push(
+            {
+               'index': this.moves.length - 1,
+               'whiteMove': this.moves[this.moves.length - 1],
+               'blackMove': "..."
+            }
+         );
+      } else {
+         movePairs.push(
+            {
+               'index': this.moves.length,
+               'whiteMove': "...",
+               'blackMove': ""
+            }
+         );
+      };
+      return  movePairs
+   }
 
-	$scope.moveControl = moveControl
-	$scope.init = function(username) {
+   $scope.moveControl = moveControl
+   $scope.init = function(username) {
         moveControl.username = username;
         moveControl.refreshMoveStatsList()
     }
