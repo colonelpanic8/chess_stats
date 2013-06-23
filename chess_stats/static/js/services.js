da7d7964a0747e258f5a89a4e1050cce72e7c0db
Livien Yin <LivienYin@gmail.com>
2013-06-23 01:45:38 -0700

    Commiting for push of submodule.

--- chess_stats/static/js/services.js
+++ chess_stats/static/js/services.js
@@ -6,3 +6,22 @@
 // Demonstrate how to register services
 // In this case it is a simple value service.
 angular.module('ChessStats.services', []);
+
+function StatsFetcher(username, color, successCallback) {
+  this.username = username;
+  this.color = color;
+  this.successCallback = successCallback;
+}
+
+StatsFetcher.prototype.fetchStatsForMoves(moves) {
+  $http({
+    'method': 'GET',
+    'url': '/get_stats',
+    'params': {
+      'moves': _.map(moves, function(move) { return move.algebraic }),
+      'username': this.username,
+      'color': this.color
+    }
+  }).success(this.successCallback);
+}
+angular.module('ChessGame.factories').factory('StatsFetcher', function() { return StatsFetcher; });
