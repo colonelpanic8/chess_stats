47f47166436c685ea56e554b673998c75dc24060
Ivan Malison <IvanMalison@gmail.com>
2013-06-23 12:44:53 -0700

    Working on fixing templating, getting things in a less crazy state. Very WIP.

--- chess_stats/static/js/services.js
+++ chess_stats/static/js/services.js
@@ -7,21 +7,23 @@
 // In this case it is a simple value service.
 angular.module('ChessStats.services', []);
 
-function StatsFetcher(username, color, successCallback) {
-  this.username = username;
-  this.color = color;
-  this.successCallback = successCallback;
-}
+angular.module('ChessStats.factories', []).factory('StatsFetcher', function($http) {
+  function StatsFetcher(username, color, successCallback) {
+    this.username = username;
+    this.color = color;
+    this.successCallback = successCallback;
+  }
 
-StatsFetcher.prototype.fetchStatsForMoves(moves) {
-  $http({
-    'method': 'GET',
-    'url': '/get_stats',
-    'params': {
-      'moves': _.map(moves, function(move) { return move.algebraic }),
-      'username': this.username,
-      'color': this.color
-    }
-  }).success(this.successCallback);
-}
-angular.module('ChessGame.factories').factory('StatsFetcher', function() { return StatsFetcher; });
+  StatsFetcher.prototype.fetchStatsForMoves = function(moves) {
+    $http({
+      'method': 'GET',
+      'url': '/get_stats',
+      'params': {
+        'moves': _.map(moves, function(move) { return move.algebraic }),
+        'username': this.username,
+        'color': this.color
+      }
+    }).success(this.successCallback);
+  }
+  return StatsFetcher;
+});
