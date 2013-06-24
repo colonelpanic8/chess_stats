58ba46be461aef67c486f6a6b3b96c2f16b50a61
Ivan Malison <IvanMalison@gmail.com>
2013-06-23 17:43:24 -0700

    Fixing move-browse page.

--- chess_stats/static/js/app.js
+++ chess_stats/static/js/app.js
@@ -2,33 +2,32 @@
 
 // Declare app level module which depends on filters, and services
 var ChessStats = angular.module(
-    'ChessStats',
-    [
-        'ChessStats.filters',
-        'ChessStats.services',
-        'ChessStats.directives',
-        'ChessStats.factories',
-        'ChessGame'
-    ]
+  'ChessStats',
+  [
+    'ChessStats.filters',
+    'ChessStats.directives',
+    'ChessStats.factories',
+    'ChessGame'
+  ]
 ).config(
-    [
-        '$routeProvider',
-        '$locationProvider',
-        function($routeProvider, $locationProvider) {
-            $locationProvider.html5Mode(true);
-        }
-    ]
+  [
+    '$routeProvider',
+    '$locationProvider',
+    function($routeProvider, $locationProvider) {
+      $locationProvider.html5Mode(true);
+    }
+  ]
 );
 
 ChessStats.directive('ngFade', function () {
-    return function (scope, element, attrs) {
-        element.css('display', 'none');
-        scope.$watch(attrs.ngFade, function (value) {
-            if (value) {
-                element.fadeIn(500);
-            } else {
-                element.fadeOut(100);
-            }
-        });
-    }
+  return function (scope, element, attrs) {
+    element.css('display', 'none');
+    scope.$watch(attrs.ngFade, function (value) {
+      if (value) {
+        element.fadeIn(500);
+      } else {
+        element.fadeOut(100);
+      }
+    });
+  }
 });
