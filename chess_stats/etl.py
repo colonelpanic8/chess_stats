9f58d8157d187196e1113a26e46581e7431c07db
Ivan Malison <IvanMalison@gmail.com>
2013-06-24 04:19:01 -0700

    Added etl to load from files. Fixed graph.

--- chess_stats/etl.py
+++ chess_stats/etl.py
@@ -1,71 +1,71 @@
 class Extractor(object):
 
-	def extract(self, identifier):
-		raise NotImplemented()
+    def extract(self, identifier):
+        raise NotImplemented()
 
 
 class Transformer(object):
 
-	def transform(self, raw_data):
-		raise NotImplemented()
+    def transform(self, raw_data):
+        raise NotImplemented()
 
 
 class Loader(object):
 
-	def load(self, transformed):
-		raise NotImplemented()
+    def load(self, transformed):
+        raise NotImplemented()
 
 
 class ETL(object):
 
-	# `extractor` should be an object that knows how to fetch data to be
-	# transformed from a single argument.
-	extractor = None
+    # `extractor` should be an object that knows how to fetch data to be
+    # transformed from a single argument.
+    extractor = None
 
-	# `transformers` should be a dictionary of transformation objects that should be
-	# applied to extracted data.
-	transformers = None
+    # `transformers` should be a dictionary of transformation objects that should be
+    # applied to extracted data.
+    transformers = None
 
-	# `loader` should be a loader instance. It should operate on a dictionary of
-	# transformed values
+    # `loader` should be a loader instance. It should operate on a dictionary of
+    # transformed values
 
-	def __init__(self, identifier):
-		self.identifier = identifier
-		self.raw_data = None
-		self.transformed = {}
+    def __init__(self, identifier):
+        self.identifier = identifier
+        self.raw_data = None
+        self.transformed = {}
 
-	def execute(self):
-		self.extract()
-		self.transform()
-		return self.load()
+    def execute(self):
+        self.extract()
+        self.transform()
+        return self.load()
 
-	def extract(self):
-		self.raw_data = self.extractor.extract(self.identifier)
-		return self.raw_data
+    def extract(self):
+        self.raw_data = self.extractor.extract(self.identifier)
+        return self.raw_data
 
-	def transform(self):
-		self.transformed.update(
-			{
-				key: transformer.transform(self.raw_data)
-				for key, transformer in self.transformers.iteritems()
-			}
-		)
+    def transform(self):
+        self.transformed.update(
+            {
+                key: transformer.transform(self.raw_data)
+                for key, transformer in self.transformers.iteritems()
+            }
+        )
 
-	def load(self):
-		return self.loader.load(self.transformed)
+    def load(self):
+        return self.loader.load(self.transformed)
 
 
 class ModelLoader(Loader):
 
-	def __init__(self, model_class):
-		self.model_class = model_class
+    def __init__(self, model_class):
+        self.model_class = model_class
 
-	def load(self, transformed):
-		model_columns = set(self.model_class.itercolumns())
-		transformed_attributes = set(transformed.keys())
-		creation_attributes = model_columns & transformed_attributes
-		model_creation_arguments = {
-			attribute_name: transformed[attribute_name]
-			for attribute_name in creation_attributes
-		}
-		return self.model_class(**model_creation_arguments)
+    def load(self, transformed):
+        model_columns = set(self.model_class.itercolumns())
+        transformed_attributes = set(transformed.keys())
+        creation_attributes = model_columns & transformed_attributes
+        model_creation_arguments = {
+            attribute_name: transformed[attribute_name]
+            for attribute_name in creation_attributes
+        }
+        return self.model_class(**model_creation_arguments)
